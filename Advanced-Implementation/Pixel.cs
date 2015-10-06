703004ce21b734882122bced856b4e8809e3aaaa
V.Jindal <vj-ug@users.noreply.github.com>
2015-10-06 16:48:12 -0500

    Create Pixel.cs

---
+++ Advanced-Implementation/Pixel.cs
@@ -0,0 +1,10 @@
+namespace Steganography
+{
+    class Pixel
+    {
+        public int A { get; set; }
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+    }
+}
